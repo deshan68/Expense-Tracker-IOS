4b7d23e0d6553162a4cadaa4b1e56eed6415f877
deshan68 <arundeshan@gmail.com>
2023-11-03 12:46:18 +0530

    Initial Commit

---
+++ Expense-Tracker/Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_TrackerApp.swift
+//  Expense-Tracker
+//
+//  Created by arun deshan on 2023-11-03.
+//
+
+import SwiftUI
+
+@main
+struct Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
